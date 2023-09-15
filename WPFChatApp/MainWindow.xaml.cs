82b8b5b969198eda444a3eae62a6682e5abc3700
GertieMeneer <gertjan.vat@icloud.com>
2023-09-15 10:44:24 +0200

    added ability to save and load msgs

--- WPFChatApp/MainWindow.xaml.cs
+++ WPFChatApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
@@ -68,6 +69,46 @@ namespace WPFChatApp
                 writer.WriteLine(message);
                 writer.Flush();
                 messageTextBox.Clear();
+            }
+        }
+
+        private void SaveMessages_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = "chat_messages.txt";
+            try
+            {
+                using (StreamWriter writer = File.CreateText(filePath))
+                {
+                    foreach (var message in chatListBox.Items)
+                    {
+                        writer.WriteLine(message.ToString());
+                    }
+                }
+                MessageBox.Show("Messages saved to file successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error saving messages", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LoadMessages_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+            openFileDialog.ShowDialog();
+            string filePath = openFileDialog.FileName;
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                string messageText = string.Join(Environment.NewLine, lines);
+                MessageBox.Show(messageText, "Loaded Messages", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("error");
             }
         }
     }
