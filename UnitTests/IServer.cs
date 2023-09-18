40e353b52fc928024e73e3dbf6987a540dcac910
Stef Kraaijeveld <itiredplease@gmail.com>
2023-09-18 19:44:10 +0200

    simple test

---
+++ UnitTests/IServer.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public interface IServer
+    {
+        public TcpListener getServer();
+    }
+}
---
+++ UnitTests/TestServer.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Moq;
+using System.Net.Sockets;
+using System.Net;
+
+namespace UnitTests
+{
+    public class TestServer
+    {
+        [Fact]
+        public void TestCreateServer()
+        {
+            // Arrange
+            var mock = new Mock<IServer>();
+            mock.Setup(test => test.getServer()).Returns(() => new TcpListener(IPAddress.Any, 12345)); // Set up the mock to return a TcpListener
+
+            // Act
+            IServer serverProvider = mock.Object;
+            TcpListener server = serverProvider.getServer();
+
+            // Assert
+            Assert.NotNull(server); // Check if the server is not null
+        }
+
+        // TODO: create client able to connect test
+
+        // TODO: logger is able to open file and write to it.
+
+        // TODO: check if server able to receive messages
+    }
+}
